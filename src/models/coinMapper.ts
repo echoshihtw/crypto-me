bd7da20745c09a9b1f6a086a84ee24eb8f1b6730
Echoooo <echoshihtw@gmail.com>
2024-01-23 17:45:31 +0800

    feat(ui): add CoinDataProps

--- src/models/coinMapper.ts
+++ src/models/coinMapper.ts
@@ -1,15 +1,37 @@
 export interface CoinItem {
   name: string;
   price: number;
-  volume_24: number;
-  volume_change_24h: number;
+  volume_24: number | undefined;
+  volume_change_24h: number | undefined;
 }
 
-export const mapCoinData = (data): CoinItem => {
+export interface CoinDataProps {
+  id: string;
+  name: string;
+  quote: {
+    USD: {
+      price: number;
+      volume_24h: number;
+      percent_change_24h: number;
+    };
+  };
+  symbol: string;
+}
+
+export function ChangeZeroToUndefined(value: number): number | undefined {
+  if (value === 0) {
+    return undefined;
+  }
+  return value;
+}
+
+export const mapCoinData = (data: CoinDataProps): CoinItem => {
   return {
     name: data.name,
     price: data.quote.USD.price,
-    volume_24: data.quote.USD.volume_24h,
-    volume_change_24h: +data.quote.USD.volume_change_24h,
+    volume_24: ChangeZeroToUndefined(data.quote.USD.volume_24h),
+    volume_change_24h: ChangeZeroToUndefined(
+      +data.quote.USD.percent_change_24h,
+    ),
   };
 };

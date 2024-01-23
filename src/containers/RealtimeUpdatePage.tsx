b3b306e1ff64d62224a8dcc800b6f1c5c100cf41
Echoooo <echoshihtw@gmail.com>
2024-01-23 15:25:59 +0800

    feat(redux): update store and get data from store

---
+++ src/containers/RealtimeUpdatePage.tsx
@@ -0,0 +1,28 @@
+import React, { memo, useEffect } from 'react';
+import CoinsOverview from '../components/CoinsOverview';
+import { selectCryptoData, selectIsLoading } from '../app/selectors/crypto';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { fetchCryptoData } from '../app/reducers/crypto';
+
+const RealtimeUpdatePage = () => {
+  const dispatch = useAppDispatch();
+  const cryptoData = useAppSelector(selectCryptoData);
+  const isLoading = useAppSelector(selectIsLoading);
+  // const error = useSelector((state: RootState) => state.crypto.error);
+
+  useEffect(() => {
+    dispatch(fetchCryptoData());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <CoinsOverview
+        coins={cryptoData}
+        title="Cryptocurrency Realtime price"
+        isLoading={isLoading}
+      />
+    </div>
+  );
+};
+
+export default memo(RealtimeUpdatePage);

9744b61db187b30ff44b078b92edf339feff46d8
Echo Shih <echoshihtw@gmail.com>
2024-01-23 15:27:48 +0800

    feat(ui): update store, router (#20)
    * feat(redux): provide store data
    
    * feat(router): add router
    
    * feat(redux): update store and get data from store
    
    * feat(ui): update coinCard component

--- src/components/CoinsOverview/CoinCard.tsx
+++ src/components/CoinsOverview/CoinCard.tsx
@@ -11,22 +11,24 @@ const CoinCard = ({ coin }: CoinCardProps) => {
   const changeDigitColor =
     volume_change_24h > 0 ? 'text-price-high' : 'text-price-low';
   return (
-    <div className="border border-grey p-1 flex flex-col rounded-[4px]">
-      <div className="text-h3 w-fit">{name}</div>
-      <div className="text-yellow text-body1 w-fit">$ {price}</div>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="flex flex-col">
-          <div className={labelClass}>volume:</div>
-          <div className={labelClass}>{volume_24}</div>
-        </div>
-        <div className="flex flex-col">
-          <div className={labelClass}>change:</div>
-          <div className={`text-body2 ${changeDigitColor} w-fit`}>
-            {volume_change_24h}
+    <>
+      <div className="border border-grey p-1 flex flex-col rounded-[4px] w-[250px] h-[118px]">
+        <div className="text-h3 w-fit">{name}</div>
+        <div className="text-yellow text-body1 w-fit">$ {price}</div>
+        <div className="flex gap-4">
+          <div className="flex flex-col">
+            <div className={labelClass}>volume:</div>
+            <div className={labelClass}>{volume_24}</div>
+          </div>
+          <div className="flex flex-col">
+            <div className={labelClass}>change:</div>
+            <div className={`text-body2 ${changeDigitColor} w-fit`}>
+              {volume_change_24h}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

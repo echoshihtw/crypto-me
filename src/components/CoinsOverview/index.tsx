8a8c0f866e282ab292652b3a12422201a29ccdaa
Echo Shih <echoshihtw@gmail.com>
2024-01-23 15:32:28 +0800

    fix: update server and add placeholder (#21)
    * feat(ui): add skeleton
    
    * feat(server): update server files

--- src/components/CoinsOverview/index.tsx
+++ src/components/CoinsOverview/index.tsx
@@ -2,22 +2,27 @@ import React, { memo } from 'react';
 import Frame from '../Frame';
 import CoinCard from './CoinCard';
 import { CoinItem } from '../../models/coinMapper';
+import CoinSkeleton from './CoinSkeleton';
 
 interface CoinsOverviewProps {
   coins: CoinItem[];
   title: string;
+  isLoading: boolean;
 }
 
-const CoinsOverview = ({ title, coins }: CoinsOverviewProps) => {
+const CoinsOverview = ({ title, coins, isLoading }: CoinsOverviewProps) => {
+  const defaultArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   return (
     <Frame
       title={title}
       content={
         <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
-          {coins?.map((coin) => <CoinCard coin={coin} key={coin.name} />)}
+          {isLoading
+            ? defaultArray.map(() => <CoinSkeleton />)
+            : coins.map((coin) => <CoinCard coin={coin} key={coin.name} />)}
         </div>
       }
-    ></Frame>
+    />
   );
 };
 

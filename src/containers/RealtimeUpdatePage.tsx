0383fe5620a997148ee757c1d2d38ca8f8172677
Echoooo <echoshihtw@gmail.com>
2024-01-24 21:11:41 +0800

    feat(ui): add container

---
+++ src/containers/RealtimeUpdatePage.tsx
@@ -0,0 +1,26 @@
+import React, { memo, useEffect } from 'react';
+import CoinsOverview from '../components/CoinsOverview';
+import { selectCryptoData, selectIsLoading } from '../app/selectors/crypto';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { fetchCryptoData } from '../app/reducers/crypto';
+
+const RealtimeUpdatePage = () => {
+  const dispatch = useAppDispatch();
+  const cryptoData = useAppSelector(selectCryptoData);
+  const isLoading = useAppSelector(selectIsLoading);
+  // const error = useSelector((state: RootState) => state.crypto.error);
+
+  useEffect(() => {
+    dispatch(fetchCryptoData());
+  }, [dispatch]);
+
+  return (
+    <CoinsOverview
+      coins={cryptoData}
+      title="Cryptocurrency Realtime price"
+      isLoading={isLoading}
+    />
+  );
+};
+
+export default memo(RealtimeUpdatePage);

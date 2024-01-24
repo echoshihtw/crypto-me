0cb79ac55613a785121a28802de3c1d71b676230
Echoooo <echoshihtw@gmail.com>
2024-01-24 23:30:11 +0800

    fix: resolve cors error and api endpoints

--- src/app/reducers/crypto.ts
+++ src/app/reducers/crypto.ts
@@ -1,60 +1,60 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { CoinItem, mapCoinData } from '../../models/coinMapper';
-import { apiEndpoints } from '../services/api';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {CoinItem, mapCoinData} from '../../models/coinMapper';
+import {apiEndpoints} from '../services/api';
 
 interface CryptoState {
-  data: CoinItem[];
-  isLoading: boolean;
-  error: string | null;
+    data: CoinItem[];
+    isLoading: boolean;
+    error: string | null;
 }
 
 const initialState: CryptoState = {
-  data: [],
-  isLoading: false,
-  error: null,
+    data: [],
+    isLoading: false,
+    error: null,
 };
 
 export const fetchCryptoData = createAsyncThunk<
-  CoinItem[],
-  void,
-  { rejectValue: string }
->('crypto/fetchData', async (_, { rejectWithValue }) => {
-  try {
-    const response = await fetch(`${apiEndpoints.getCryptoData}`);
-    const { data } = await response.json();
-    const mappedData = data.map((item: any) => mapCoinData(item));
-    return mappedData;
-  } catch (error) {
-    return rejectWithValue('Error fetching crypto data');
-  }
+    CoinItem[],
+    void,
+    { rejectValue: string }
+>('crypto/fetchData', async (_, {rejectWithValue}) => {
+    try {
+        const response = await fetch(`${apiEndpoints.getCryptoData}`);
+        const {data} = await response.json();
+        const mappedData = data.map((item: any) => mapCoinData(item));
+        return mappedData;
+    } catch (error) {
+        return rejectWithValue('Error fetching crypto data');
+    }
 });
 
 const cryptoSlice = createSlice({
-  name: 'crypto',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchCryptoData.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(
-        fetchCryptoData.fulfilled,
-        (state, action: PayloadAction<CoinItem[]>) => {
-          state.isLoading = false;
-          state.data = action.payload;
-          state.error = null;
-        },
-      )
-      .addCase(
-        fetchCryptoData.rejected,
-        (state, action: PayloadAction<string | undefined>) => {
-          state.isLoading = false;
-          state.error = action.payload || 'An error occurred';
-        },
-      );
-  },
+    name: 'crypto',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCryptoData.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(
+                fetchCryptoData.fulfilled,
+                (state, action: PayloadAction<CoinItem[]>) => {
+                    state.isLoading = false;
+                    state.data = action.payload;
+                    state.error = null;
+                },
+            )
+            .addCase(
+                fetchCryptoData.rejected,
+                (state, action: PayloadAction<string | undefined>) => {
+                    state.isLoading = false;
+                    state.error = action.payload || 'An error occurred';
+                },
+            );
+    },
 });
 
 export default cryptoSlice.reducer;

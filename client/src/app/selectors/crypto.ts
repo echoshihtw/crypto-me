52e4baa768140671f22a76f49a5a7e4ccd3fea39
Echo Shih <echoshihtw@gmail.com>
2024-01-30 02:53:03 +0800

    Feat: deployment clean up(#41)
    * refactor: move client to /client
    
    * feat: add Root package.json
    
    * feat: add script for client and server
    
    * feat: update server root
    
    * feat: add root script install:all

--- client/src/app/selectors/crypto.ts
+++ client/src/app/selectors/crypto.ts
@@ -1,8 +1,5 @@
-// src/selectors/cryptoSelectors.ts
-
 import { RootState } from '../../configureStore';
 
-// Selectors
 export const selectCryptoData = (state: RootState) => state.crypto.data;
 export const selectIsLoading = (state: RootState) => state.crypto.isLoading;
 export const getCryptoError = (state: RootState) => state.crypto.error;

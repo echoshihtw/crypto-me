18e378fd5d231207356c7fc57410b7c4b17a8a1a
Echoooo <echoshihtw@gmail.com>
2024-01-31 00:58:03 +0800

    fix(server): resolve CORS from client

--- server/src/sockets/socketHandler.ts
+++ server/src/sockets/socketHandler.ts
@@ -2,7 +2,12 @@ import { Server as SocketIOServer } from "socket.io";
 import http from "http";
 
 export function setupSocketServer(server: http.Server) {
-  const io = new SocketIOServer(server);
+  const io = new SocketIOServer(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
 
   io.on("connection", (socket) => {
     console.log("a user connected:", socket.id);

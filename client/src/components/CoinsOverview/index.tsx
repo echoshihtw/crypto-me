e235bbc1f1ebd179bdffa613f95df2ca6df0f759
Echoooo <echoshihtw@gmail.com>
2024-01-31 13:40:36 +0800

    fix: rewrite default array

--- client/src/components/CoinsOverview/index.tsx
+++ client/src/components/CoinsOverview/index.tsx
@@ -10,21 +10,26 @@ export interface CoinsOverviewProps {
   isLoading: boolean;
 }
 
+const SKELETON_COUNT = 10;
+const defaultArray = Array.from(
+  { length: SKELETON_COUNT },
+  (_, index) => index,
+);
+
 const CoinsOverview = ({ title, coins, isLoading }: CoinsOverviewProps) => {
-  const defaultArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   return (
-      <div className="h-full w-full">
-          <Frame
-              title={title}
-              content={
-                  <div className="grid grid-cols-1 place-items-center gap-2 sm:grid-cols-2 lg:grid-cols-3">
-                      {isLoading || coins?.length < 1
-                          ? defaultArray.map((index) => <CoinSkeleton key={index}/>)
-                          : coins?.map((coin) => <CoinCard coin={coin} key={coin.name}/>)}
-                  </div>
-              }
-          />
-      </div>
+    <div className="h-full w-full">
+      <Frame
+        title={title}
+        content={
+          <div className="grid grid-cols-1 place-items-center gap-2 sm:grid-cols-2 lg:grid-cols-3">
+            {isLoading || coins?.length < 1
+              ? defaultArray.map((index) => <CoinSkeleton key={index} />)
+              : coins?.map((coin) => <CoinCard coin={coin} key={coin.name} />)}
+          </div>
+        }
+      />
+    </div>
   );
 };
 

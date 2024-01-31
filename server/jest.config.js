eb40dc67e37260654cdc60cbee562a1e674df672
Echoooo <echoshihtw@gmail.com>
2024-01-31 15:41:35 +0800

    fix: remove deprecated globals section from jest configuration

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,16 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src/tests"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testMatch: ["**/tests/**/*.test.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/src/tests"],
+    testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+    transform: {
+        "^.+\\.tsx?$": ['ts-jest', {
+            tsconfig: 'tsconfig.json'
+        }],
     },
-  },
+    testMatch: ["**/tests/**/*.test.ts"],
 };

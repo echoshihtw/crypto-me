8c29b7531d626a66a18c94e876f2a1662fecf858
Echoooo <echoshihtw@gmail.com>
2024-02-03 07:22:53 +0800

    fix(server): only fetch data when cryptoPrices is null or outdated

--- server/src/sockets/socketHandler.ts
+++ server/src/sockets/socketHandler.ts
@@ -1,23 +1,30 @@
 import { Server as SocketIOServer } from "socket.io";
 import http from "http";
-import { fetchCryptoPrices } from "../services/apiService";
+import { cryptoPrices, getCryptoPrices, lastUpdated } from "../server";
 
 export function setupSocketServer(server: http.Server) {
   const io = new SocketIOServer(server, {
     cors: {
       origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
+      methods: ["GET"],
     },
   });
 
-  io.on("connection", (socket) => {
-    console.log("a user connected:", socket.id);
-    fetchCryptoPrices().then((data) => {
-      socket.emit("cryptoPricesUpdate", data);
-    });
+  const cryptoPriceNamespace = io.of("/crypto-price");
+
+  io.of("/crypto-price").on("connection", async (socket) => {
+    socket.join("ticker");
+    console.log(`${Date.now()}: ${socket.id} connected`);
+
+    if (!cryptoPrices || lastUpdated - Date.now() > 60000) {
+      await getCryptoPrices();
+    }
+
+    cryptoPriceNamespace.to("ticker").emit("current-price", cryptoPrices);
+    console.log(`${Date.now()}: ${socket.id} emitted current-price`);
 
     socket.on("disconnect", () => {
-      console.log("user disconnected");
+      console.log(`${Date.now()}: ${socket.id} disconnected`);
     });
   });
 

8ef3e68bb262394f4b44f066d9646f4d654df96f
Echoooo <echoshihtw@gmail.com>
2024-02-03 23:54:45 +0800

    feat(server): unit test for app and server

--- server/src/tests/server.test.ts
+++ server/src/tests/server.test.ts
@@ -1,24 +1,24 @@
 import request from "supertest";
 import http from "http";
-import { app } from "../server";
-import { fetchCryptoPrices } from "../services/apiService";
+import { app } from "../app";
 
-jest.mock("../services/apiService"); // Mock the apiService
-
-describe("App", () => {
-  let server: http.Server;
-
-  beforeAll(() => {
-    server = http.createServer(app);
-    server.listen();
+describe("Server", () => {
+  let httpServer: http.Server;
+  beforeAll((done) => {
+    httpServer = http.createServer(app);
+    httpServer.listen(() => {
+      done();
+    });
   });
 
   afterAll((done) => {
-    server.close(done);
+    httpServer.close(() => {
+      done();
+    });
   });
 
   it("should start the HTTP server without errors", async () => {
-    const response = await request(server).get("/");
+    const response = await request(httpServer).get("/");
     expect(response.headers["access-control-allow-origin"]).toBeDefined();
   });
 });
